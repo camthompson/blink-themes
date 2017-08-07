317709a4b33888124dd41f589133fa03f21af95c
Cam Thompson <cam@camthompson.com>
2017-08-07 17:08:23 -0400

    Add onedark theme

---
+++ themes/onedark.js
@@ -0,0 +1,21 @@
+t.prefs_.set('color-palette-overrides',
+  ["#282c34",
+    "#e06c75",
+    "#98c379",
+    "#e5c07b",
+    "#61afef",
+    "#c678dd",
+    "#56b6c2",
+    "#abb2bf",
+    "#3e4452",
+    "#be5046",
+    "#98c379",
+    "#d19a66",
+    "#61afef",
+    "#c678dd",
+    "#56b6c2",
+    "#5c6670"]);
+
+t.prefs_.set('foreground-color', "#abb2c0");
+t.prefs_.set('background-color', "#282c34");
+t.prefs_.set('cursor-color', 'rgba(199,199,199,0.5)');
